7994f5e7190fee7f25b3116b46ec257fdf357df7
Wanjiku <Wanjiku>
2019-05-09 16:02:46 +0300

    Created a model for categories

--- photos/models.py
+++ photos/models.py
@@ -4,6 +4,9 @@ class Editor(models.Model):
     name = models.CharField(max_length = 30)
     email = models.EmailField()
 
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
     image =models.ImageField()
     name = models.CharField(max_length = 30)
@@ -11,3 +14,8 @@ class Image(models.Model):
     location = models.CharField(max_length = 30)
     category = models.CharField(max_length = 30)
 
+class category(models.Model):
+    name=models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
