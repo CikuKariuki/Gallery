909ce5c646268a7c9e085bbff76b418df22bb2c1
Wanjiku <Wanjiku>
2019-05-10 11:25:27 +0300

    added models to my admin page

--- photos/admin.py
+++ photos/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
-from 
+from .models import Editor,Image,Category,Location
+
+admin.site.register(Editor)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Location)
+
